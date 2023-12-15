faa5b39f4e1d5de9e40242ef0c2a4f7efd053548
Silyphin <138956125+Silyphin@users.noreply.github.com>
2023-12-15 20:00:04 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//auto slider
+// Auto slider
 let index = 0;
 displayImages();
+
 function displayImages() {
   let i;
   const images = document.getElementsByClassName("image");
@@ -11,6 +12,38 @@ function displayImages() {
   if (index > images.length) {
     index = 1;
   }
-  images[index-1].style.display = "block";
-  setTimeout(displayImages, 5000); 
+  images[index - 1].style.display = "block";
+  setTimeout(displayImages, 10000);
+}
+
+// Manual slider
+let slideIndex = 1;
+showSlides(index);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("image");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
